a833926c700056ed775ab72b21b9cbdb209dfe6e
андрей <p.aleksey2010@yandex.ru>
2023-03-14 16:18:49 +0300

    .\MidleImportant.java

---
+++ MidleImportant.java
@@ -0,0 +1,11 @@
+public class MidleImportant extends Task {
+
+    public MidleImportant(LocalDate data, String node, String deadLineTime, String deadLineDate, String avtor) {
+        super(data, node, deadLineTime, deadLineDate, avtor);
+    }
+
+    @Override
+    public String toString() {
+        return "! Midle " + super.toString();
+    }
+}
