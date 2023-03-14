dac269a77773434a9d108972673f448200acd98c
андрей <p.aleksey2010@yandex.ru>
2023-03-14 15:59:01 +0300

    .\Export.java

---
+++ Export.java
@@ -0,0 +1,9 @@
+public class Export implements Serializable {
+    public void doExsport(NotePad np) throws IOException {
+        FileOutputStream fos = new FileOutputStream("MyNotePad.out");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(np);
+        oos.flush();
+        oos.close();
+    }
+}
