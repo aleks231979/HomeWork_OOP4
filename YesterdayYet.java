612257c6de6eb294012e85529b2bcf0d4acbf9c0
андрей <p.aleksey2010@yandex.ru>
2023-03-14 16:40:40 +0300

    .\YesterdayYet.java

---
+++ YesterdayYet.java
@@ -0,0 +1,11 @@
+public class YesterdayYet extends Task {
+
+    public YesterdayYet(LocalDate data, String node, String deadLineTime, String deadLineDate, String avtor) {
+        super(data, node, deadLineTime, deadLineDate, avtor);
+    }
+
+    @Override
+    public String toString() {
+        return "!!! High " + super.toString();
+    }
+}
